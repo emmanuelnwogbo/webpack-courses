e33a8e128e3c4cf9c1288f458473a6585d4188af
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-09-04 10:47:26 +0100

    code splitting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import sum from './sum';
+/*import sum from './sum';
 import './image_viewer';
-//we're pretty much just importing the entire file above
+//we're pretty much just importing the entire image_viewer.js above
 
 const total = sum(10, 5);
-console.log(total)
+console.log(total)*/
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+
+button.onclick = () => {
+  //implementing code splitting
+  System.import('./image_viewer').then(module => {
+    module.default();
+  })
+};
+
+document.body.appendChild(button);
